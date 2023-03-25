08b4da2a01d2f8fe18425d79668cb41b2211765d
Ramy Ashraf <Ramy3r@hotmail.com>
2023-03-25 23:14:57 +0200

    added motion layout for current day weather info and some other ui enhancements

--- app/src/main/java/com/example/skywise/Constants.kt
+++ app/src/main/java/com/example/skywise/Constants.kt
@@ -5,13 +5,7 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
 /*
-* https://pro.openweathermap.org/data/2.5/onecall?
-* lat=33.44
-* &lon=-94.04
-* &lang=ar
-* &units=metric
-* &exclude=minutely,hourly,daily
-* &appid=0700dd38f51000c5455a7693bc132580
+* https://pro.openweathermap.org/data/2.5/onecall?lat=31.0&lon=29.800&lang=ar&units=metric&exclude=minutely,hourly,daily&appid=0700dd38f51000c5455a7693bc132580
 * */
 const val BASE_URL = "https://pro.openweathermap.org"
 const val AR = "ar"
--- app/src/main/java/com/example/skywise/data/Repository.kt
+++ app/src/main/java/com/example/skywise/data/Repository.kt
@@ -1,14 +1,11 @@
 package com.example.skywise.data
 
-import com.example.skywise.API_KEY
-import com.example.skywise.AR
-import com.example.skywise.METRIC
-import com.example.skywise.MINUTELY
+import com.example.skywise.*
 import com.example.skywise.data.remotesource.RetrofitClient
 
 class Repository(private val retrofit: RetrofitClient) {
     suspend fun getLatestData(): WeatherData {
-        return retrofit.api.oneCall(31.0, 29.800, AR, METRIC, listOf(MINUTELY), API_KEY)
+        return retrofit.api.oneCall(31.0, 29.800, EN, METRIC, listOf(MINUTELY), API_KEY)
             .body() as WeatherData
     }
 }
